0942509ca42c328e1f6fe5ec9a840b54f35ed6af
Philippe Vinchon <p.vinchon@gmail.com>
2026-02-11 21:28:11 +0000

    Implementation

--- _config.ts
+++ _config.ts
@@ -1,6 +1,7 @@
 import lume from "lume/mod.ts";
 import basePath from "lume/plugins/base_path.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
+import processor from "./src/pwa/processor.ts";
 
 const site = lume({ src: "./src" });
 
@@ -8,5 +9,10 @@ site.use(tailwindcss({ minify: true }));
 site.use(basePath());
 
 site.add("style.css");
+site.add("pwa/src/service_worker.js", "service_worker.js");
+site.add("pwa/src/manifest.webmanifest", "manifest.webmanifest");
+site.add("pwa/src/icons", "icons");
+
+site.process([".js"], (pages) => processor(site, pages));
 
 export default site;
